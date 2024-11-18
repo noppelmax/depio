945f642ccb0c2026720b809df371e4f6947c0d6b
noppelmax <max@noppelmax.online>
2024-11-18 11:22:38 +0100

    Extend the demo file. Add a failing job

--- demo.py
+++ demo.py
@@ -21,6 +21,8 @@ def funcdec(input: Annotated[pathlib.Path, Dependency],
             ):
     print(f"func dec reading from {input} and writing to {output}")
     time.sleep(sec)
+    if input == BLD/"output1.txt":
+        raise Exception("Demo exception")
     with open(output,'w') as f:
         f.write("Hallo from depio")
     return 1
@@ -31,5 +33,8 @@ BLD.mkdir(exist_ok=True)
 
 funcdec(BLD/"output.txt", BLD/"final.txt",sec=3)
 funcdec(BLD/"input.txt", BLD/"output.txt",sec=5)
+funcdec(BLD/"input.txt", BLD/"output1.txt",sec=2)
+funcdec(BLD/"input.txt", BLD/"output2.txt",sec=1)
+funcdec(BLD/"output1.txt", BLD/"final1.txt",sec=1)
 
 taskhandler.run()
