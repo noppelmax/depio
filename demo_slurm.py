0c3aedb280ec7e87deb555c448709af3309b4323
noppelmax <max@noppelmax.online>
2024-11-18 15:08:36 +0100

    Implement the parameters of the slurm job

--- demo_slurm.py
+++ demo_slurm.py
@@ -1,12 +1,29 @@
+import os
 from typing import Annotated
 import pathlib
+import submitit
+
 from depio.Executors import SubmitItExecutor
 from depio.TaskHandler import TaskHandler
 from depio.decorators import task
 from depio.Task import Product, Dependency
 
-executor = SubmitItExecutor()
-taskhandler = TaskHandler(executor=executor)
+BLD = pathlib.Path("build")
+BLD.mkdir(exist_ok=True)
+
+SLURM = pathlib.Path("slurm")
+SLURM.mkdir(exist_ok=True)
+
+internal_executor = submitit.AutoExecutor(folder=SLURM)
+os.environ["SBATCH_RESERVATION"] = "isec-team"
+internal_executor.update_parameters(
+            time="48:00:00",
+            partition="gpu",
+            mem_gb=32
+)
+
+depioExecutor = SubmitItExecutor(internal_executor=internal_executor)
+taskhandler = TaskHandler(depioExecutor=depioExecutor)
 
 # Use the decorator with args and kwargs
 @task("datapipeline", taskhandler)
@@ -18,8 +35,6 @@ def funcdec(input: Annotated[pathlib.Path, Dependency],
     #    f.write("Hallo from depio")
 
 
-BLD = pathlib.Path("build")
-BLD.mkdir(exist_ok=True)
 
 funcdec(BLD/"output.txt", BLD/"final.txt")
 funcdec(BLD/"input.txt", BLD/"output.txt")
