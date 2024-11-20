67f68417d5b9432f8ffef05393985be33accbe39
noppelmax <max@noppelmax.online>
2024-11-20 10:01:26 +0100

    Update demo with optional input

--- demo.py
+++ demo.py
@@ -17,13 +17,14 @@ defaultpipeline = Pipeline(depioExecutor=depioExecutor, clear_screen=False)
 
 # Use the decorator with args and kwargs
 @task("datapipeline")
-def funcdec(input: Annotated[pathlib.Path, Dependency],
-            output: Annotated[pathlib.Path, Product],
-            sec:int
+def funcdec(
+        output: Annotated[pathlib.Path, Product],
+        sec: int,
+        input: Annotated[pathlib.Path, Dependency] = None
             ):
     print(f"func dec reading from {input} and writing to {output}")
     time.sleep(sec)
-    if input == BLD/"output1.txt":
+    if False and input == BLD/"output1.txt":
         raise Exception("Demo exception")
     with open(output,'w') as f:
         f.write("Hallo from depio")
@@ -33,11 +34,10 @@ def funcdec(input: Annotated[pathlib.Path, Dependency],
 BLD = pathlib.Path("build")
 BLD.mkdir(exist_ok=True)
 
-#funcdec(BLD/"output.txt", BLD/"final.txt",sec=3)
-#funcdec(BLD/"input.txt", BLD/"output.txt",sec=5)
-
-defaultpipeline.add_task(funcdec(BLD/"input.txt", BLD/"output1.txt",sec=2))
-defaultpipeline.add_task(funcdec(BLD/"output1.txt", BLD/"final1.txt",sec=1))
-defaultpipeline.add_task(funcdec(BLD/"final1.txt", BLD/"final_final1.txt",sec=2))
+defaultpipeline.add_task(funcdec(BLD/"output1.txt",sec=2,input=BLD/"input.txt"))
+t1 = defaultpipeline.add_task(funcdec(BLD/"output2.txt",sec=1,input=BLD/"input.txt"))
+defaultpipeline.add_task(funcdec(BLD/"final1.txt",sec=1,input=BLD/"output1.txt"))
+defaultpipeline.add_task(funcdec(BLD/"final_final1.txt",sec=2,input=BLD/"final1.txt"))
+defaultpipeline.add_task(funcdec(output=BLD/"final_final2.txt",sec=2))
 
 exit(defaultpipeline.run())
