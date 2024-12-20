9862ca2d1f5461ef929773daa6d04ac9df2e56c8
noppelmax <max@noppelmax.online>
2024-12-20 18:37:57 +0100

    Format

--- src/depio/Task.py
+++ src/depio/Task.py
@@ -384,8 +384,7 @@ class Task:
 
     def __eq__(self, other):
         if isinstance(other, self.__class__):
-            return (self.func == other.func
-                    and self.cleaned_args == other.cleaned_args)
+            return self.func == other.func and self.cleaned_args == other.cleaned_args
         else:
             return False
 
